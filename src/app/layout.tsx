9657537c9f7bf449b3aa1bc3139d1a5d52f0a745
Archana G <archanaarchana6463536277@gmail.com>
2026-03-06 15:18:14 +0530

    fix: revert to standard next build to fix vercel 126 error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
-// import "@/app/globals.css";
+import "@/app/globals.css";
 
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -17,10 +17,8 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <head>
-                <link rel="stylesheet" href="/styles.css" />
             </head>
             <body className="font-sans">{children}</body>
-        </html>
+        </html >
     );
 }
