3e6913415ad2ca05c2b6c6cae9bc2c6b1e3901f5
Archana G <archanaarchana6463536277@gmail.com>
2026-03-09 16:28:00 +0530

    First commit

--- next.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  ignoreDuringBuilds: true
- 
-};
-
-export default nextConfig;
-
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl ="https://nbqxypxuwozrkdlwevye.supabase.co";
+const supabaseAnonKey ="sb_secret_W-olhaQ8pOlro6imWFRnTA_oVePkyc4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
